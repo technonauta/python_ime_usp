7055579eb42afef77d2000cabb37027720948d85
technonauta <118274082+technonauta@users.noreply.github.com>
2022-12-31 11:02:57 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,6 @@
+#https://www.ime.usp.br/~macmulti/exercicios/repetenc/index.html
+#Exercícios com Repetições Encaixadas
+#1.  Dados n e n seqüências de números inteiros não-nulos, cada 
+# qual seguida por um 0, calcular a soma dos números pares de cada seqüência.
+
+
